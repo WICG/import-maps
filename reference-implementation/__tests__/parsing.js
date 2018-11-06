55dd0aafab45fe96268e67ba8a7ca553ed38f0be
Domenic Denicola <d@domenic.me>
2018-11-06 15:45:44 -0500

    Test specifier maps both inside imports and scopes

--- reference-implementation/__tests__/parsing.js
+++ reference-implementation/__tests__/parsing.js
@@ -7,6 +7,14 @@ function expectBad(input) {
   expect(() => parseFromString(input)).toThrow(TypeError);
 }
 
+function expectSpecifierMap(input, output) {
+  expect(parseFromString(`{ "imports": ${input} }`))
+    .toEqual({ imports: output, scopes: {} });
+
+  expect(parseFromString(`{ "scopes": { "aScope":  ${input} } }`))
+    .toEqual({ imports: {}, scopes: { aScope: output } });
+}
+
 test('Invalid JSON', () => {
   expect(() => parseFromString('{ imports: {} }')).toThrow(SyntaxError);
 });
@@ -43,78 +51,39 @@ describe('Mismatching the specifier map schema', () => {
   const invalidMapTargetStrings = ['null', 'true', '1', '{}'];
   const invalidInsideArrayStrings = ['null', 'true', '1', '{}', '[]'];
 
-  describe('Directly under imports', () => {
-    it('should ignore entries where the map target is not a string or array', () => {
-      for (const invalid of invalidMapTargetStrings) {
-        expect(parseFromString(`{
-          "imports": {
-            "foo": ${invalid},
-            "bar": ["./valid"]
-          }
-        }`))
-          .toEqual({ imports: { bar: ['./valid'] }, scopes: {} });
-      }
-    });
-
-    it('should ignore members of a map target array that are not strings', () => {
-      for (const invalid of invalidInsideArrayStrings) {
-        expect(parseFromString(`{
-          "imports": {
-            "foo": ["./valid", ${invalid}],
-            "bar": ["./valid"]
-          }
-        }`))
-          .toEqual({ imports: {
-            foo: ['./valid'],
-            bar: ['./valid']
-          }, scopes: {} });
-      }
-    });
+  it('should ignore entries where the map target is not a string or array', () => {
+    for (const invalid of invalidMapTargetStrings) {
+      expectSpecifierMap(
+        `{
+          "foo": ${invalid},
+          "bar": ["./valid"]
+        }`,
+        {
+          bar: ['./valid']
+        }
+      );
+    }
   });
 
-  describe('Underneath a scope', () => {
-    it('should throw if the scope value is not an object', () => {
-      for (const invalid of nonObjectStrings) {
-        expectBad(`{ "scopes": { "someScope": ${invalid} } }`);
-      }
-    });
+  it('should ignore members of a map target array that are not strings', () => {
+    for (const invalid of invalidInsideArrayStrings) {
+      expectSpecifierMap(
+        `{
+          "foo": ["./valid", ${invalid}],
+          "bar": ["./valid"]
+        }`,
+        {
+          foo: ['./valid'],
+          bar: ['./valid']
+        }
+      );
+    }
+  });
 
-    it('should ignore entries where the map target is not a string or array', () => {
-      for (const invalid of invalidMapTargetStrings) {
-        expect(parseFromString(`{
-          "scopes": {
-            "someScope": {
-              "foo": ${invalid},
-              "bar": ["./valid"]
-            }
-          }
-        }`))
-          .toEqual({ imports: {}, scopes: {
-            someScope: {
-              bar: ['./valid']
-            }
-          } });
-      }
-    });
-
-    it('should ignore members of a map target array that are not strings', () => {
-      for (const invalid of invalidInsideArrayStrings) {
-        expect(parseFromString(`{
-          "scopes": {
-            "someScope": {
-              "foo": ["./valid", ${invalid}],
-              "bar": ["./valid"]
-            }
-          }
-        }`))
-          .toEqual({ imports: {}, scopes: {
-            someScope: {
-              foo: ['./valid'],
-              bar: ['./valid']
-            }
-          } });
-      }
-    });
+  it('should throw if the scope value is not an object', () => {
+    for (const invalid of nonObjectStrings) {
+      expectBad(`{ "scopes": { "someScope": ${invalid} } }`);
+    }
   });
 });
 
@@ -131,71 +100,54 @@ describe('Normalization', () => {
     expect(parseFromString(`{ "imports": {} }`)).toEqual({ imports: {}, scopes: {} });
   });
 
-  it('should normalize map targets to arrays inside "imports"', () => {
-    expect(parseFromString(`{
-      "imports": {
+  it('should normalize map targets to arrays', () => {
+    expectSpecifierMap(
+      `{
         "foo": "./valid1",
         "bar": ["./valid2"]
-      }
-    }`))
-      .toEqual({ imports: {
+      }`,
+      {
         foo: ['./valid1'],
         bar: ['./valid2']
-      }, scopes: {} });
-  });
-
-  it('should normalize map targets to arrays inside scopes', () => {
-    expect(parseFromString(`{
-      "scopes": {
-        "aScope": {
-          "foo": "./valid1",
-          "bar": ["./valid2"]
-        }
       }
-    }`))
-      .toEqual({ imports: {}, scopes: {
-        aScope: {
-          foo: ['./valid1'],
-          bar: ['./valid2']
-        }
-      } });
+    );
   });
 });
 
 describe('Map target string validation', () => {
   it('should accept strings prefixed with ./, ../, or /', () => {
-    expect(parseFromString(`{
-      "imports": {
+    expectSpecifierMap(
+      `{
         "dotSlash": "./foo",
         "dotDotSlash": "../foo",
         "slash": "/foo"
-      }
-    }`))
-      .toEqual({ imports: {
+      }`,
+      {
         dotSlash: ['./foo'],
         dotDotSlash: ['../foo'],
         slash: ['/foo']
-      }, scopes: {} });
+      }
+    );
   });
 
   it('should accept the literal strings ./, ../, or / with no suffix', () => {
-    expect(parseFromString(`{
-      "imports": {
+    expectSpecifierMap(
+      `{
         "dotSlash": "./",
         "dotDotSlash": "../",
         "slash": "/"
-      }
-    }`))
-      .toEqual({ imports: {
+      }`,
+      {
         dotSlash: ['./'],
         dotDotSlash: ['../'],
         slash: ['/']
-      }, scopes: {} });
+      }
+    );
   });
 
   it('should ignore percent-encoded variants of ./, ../, or /', () => {
-    expect(parseFromString(`{
-      "imports": {
+    expectSpecifierMap(
+      `{
         "dotSlash1": "%2E/",
         "dotDotSlash1": "%2E%2E/",
         "dotSlash2": ".%2F",
@@ -203,9 +155,8 @@ describe('Map target string validation', () => {
         "slash2": "%2F",
         "dotSlash3": "%2E%2F",
         "dotDotSlash3": "%2E%2E%2F"
-      }
-    }`))
-      .toEqual({ imports: {
+      }`,
+      {
         dotSlash1: [],
         dotDotSlash1: [],
         dotSlash2: [],
@@ -213,47 +164,48 @@ describe('Map target string validation', () => {
         slash2: [],
         dotSlash3: [],
         dotDotSlash3: []
-      }, scopes: {} });
+      }
+    );
   });
 
   it('should accept strings prefixed with the built-in module prefix', () => {
-    expect(parseFromString(`{
-      "imports": {
+    expectSpecifierMap(
+      `{
         "foo": "${BUILT_IN_MODULE_PREFIX}foo"
-      }
-    }`))
-      .toEqual({ imports: {
+      }`,
+      {
         foo: [`${BUILT_IN_MODULE_PREFIX}foo`]
-      }, scopes: {} });
+      }
+    );
   });
 
   it('should ignore percent-encoded variants of the built-in module prefix', () => {
-    expect(parseFromString(`{
-      "imports": {
+    expectSpecifierMap(
+      `{
         "foo": "${encodeURIComponent(BUILT_IN_MODULE_PREFIX)}foo"
-      }
-    }`))
-      .toEqual({ imports: {
+      }`,
+      {
         foo: []
-      }, scopes: {} });
+      }
+    );
   });
 
   it('should ignore other unprefixed strings', () => {
     for (const bad of ['bar', '\\bar', '~bar', '#bar', '?bar']) {
-      expect(parseFromString(`{
-        "imports": {
+      expectSpecifierMap(
+        `{
           "foo": "${bad}"
-        }
-      }`))
-        .toEqual({ imports: {
+        }`,
+        {
           foo: []
-        }, scopes: {} });
+        }
+      );
     }
   });
 
   it('should only accept URL-string map targets with fetch schemes', () => {
-    expect(parseFromString(`{
-      "imports": {
+    expectSpecifierMap(
+      `{
         "about": "about:good",
         "blob": "blob:good",
         "data": "data:good",
@@ -266,9 +218,8 @@ describe('Map target string validation', () => {
         "mailto": "mailto:bad",
         "javascript": "javascript:bad",
         "wss": "wss:bad"
-      }
-    }`))
-      .toEqual({ imports: {
+      }`,
+      {
         about: ['about:good'],
         blob: ['blob:good'],
         data: ['data:good'],
@@ -281,12 +232,13 @@ describe('Map target string validation', () => {
         mailto: [],
         javascript: [],
         wss: []
-      }, scopes: {} });
+      }
+    );
   });
 
   it('should only accept URL-string map targets with fetch schemes inside arrays', () => {
-    expect(parseFromString(`{
-      "imports": {
+    expectSpecifierMap(
+      `{
         "about": ["./valid1", "about:good", "../valid2"],
         "blob": ["./valid1", "blob:good", "../valid2"],
         "data": ["./valid1", "data:good", "../valid2"],
@@ -299,9 +251,8 @@ describe('Map target string validation', () => {
         "mailto": ["./valid1", "mailto:bad", "../valid2"],
         "javascript": ["./valid1", "javascript:bad", "../valid2"],
         "wss": ["./valid1", "wss:bad", "../valid2"]
-      }
-    }`))
-      .toEqual({ imports: {
+      }`,
+      {
         about: ['./valid1', 'about:good', '../valid2'],
         blob: ['./valid1', 'blob:good', '../valid2'],
         data: ['./valid1', 'data:good', '../valid2'],
@@ -314,6 +265,7 @@ describe('Map target string validation', () => {
         mailto: ['./valid1', '../valid2'],
         javascript: ['./valid1', '../valid2'],
         wss: ['./valid1', '../valid2']
-      }, scopes: {} });
+      }
+    );
   });
 });
