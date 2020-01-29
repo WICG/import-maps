6cb173dc44449e1b341e49c29509867150f670d9
hiroshige-g <hiroshige@chromium.org>
2020-01-29 14:07:44 -0800

    Reference implementation: migrate to JSON-based tests
    In order to share common structure with Web Platform Tests, this PR migrates hard-coded Jest tests into JSON test metadata (__tests__/json/*.json, which are shared with
    https://github.com/web-platform-tests/wpt/tree/master/import-maps/common/resources)
    and test runners (__tests__/helpers/, which are Jest-specific).
    
    In the future we will likely use a script to download the json/ directory and not check it in to this repository, in order to maintain a single source of truth in the web platform tests repository.

---
+++ reference-implementation/__tests__/helpers/common-test-helper.js
@@ -0,0 +1,129 @@
+'use strict';
+const assert = require('assert');
+const { URL } = require('url');
+const { parseFromString } = require('../../lib/parser.js');
+const { resolve } = require('../../lib/resolver.js');
+
+function assertNoExtraProperties(object, expectedProperties, description) {
+  for (const actualProperty in object) {
+    assert(
+      expectedProperties.indexOf(actualProperty) !== -1,
+      description + ': unexpected property ' + actualProperty
+    );
+  }
+}
+
+function assertOwnProperty(j, name) {
+  assert(name in j);
+}
+
+// Parsed import maps in the reference implementation uses `URL`s instead of
+// strings as the values of specifier maps, while
+// expected import maps (taken from JSONs) uses strings.
+// This function converts `m` (expected import maps or its part)
+// into URL-based, for comparison.
+function replaceStringWithURL(m) {
+  if (typeof m === 'string') {
+    return new URL(m);
+  }
+  if (m === null || typeof m !== 'object') {
+    return m;
+  }
+
+  const result = {};
+  for (const key in m) {
+    result[key] = replaceStringWithURL(m[key]);
+  }
+  return result;
+}
+
+function runTests(j) {
+  const { tests } = j;
+  delete j.tests;
+
+  if ('importMap' in j) {
+    assertOwnProperty(j, 'importMap');
+    assertOwnProperty(j, 'importMapBaseURL');
+    try {
+      j.parsedImportMap = parseFromString(
+        JSON.stringify(j.importMap),
+        new URL(j.importMapBaseURL)
+      );
+    } catch (e) {
+      j.parsedImportMap = e;
+    }
+    delete j.importMap;
+    delete j.importMapBaseURL;
+  }
+
+  assertNoExtraProperties(
+    j,
+    [
+      'expectedResults', 'expectedParsedImportMap',
+      'baseURL', 'name', 'parsedImportMap',
+      'importMap', 'importMapBaseURL',
+      'link', 'details'
+    ],
+    j.name
+  );
+
+  if (tests) {
+    // Nested node.
+    for (const testName in tests) {
+      let fullTestName = testName;
+      if (j.name) {
+        fullTestName = j.name + ': ' + testName;
+      }
+      tests[testName].name = fullTestName;
+      const k = Object.assign(Object.assign({}, j), tests[testName]);
+      runTests(k);
+    }
+  } else {
+    // Leaf node.
+    for (const key of ['parsedImportMap', 'name']) {
+      assertOwnProperty(j, key, j.name);
+    }
+    assert(
+      'expectedResults' in j ||
+           'expectedParsedImportMap' in j,
+      'expectedResults or expectedParsedImportMap should exist'
+    );
+
+    // Resolution tests.
+    if ('expectedResults' in j) {
+      it(j.name, () => {
+        assertOwnProperty(j, 'baseURL');
+        describe(
+          'Import map registration should be successful for resolution tests',
+          () => {
+            expect(j.parsedImportMap).not.toBeInstanceOf(Error);
+          }
+        );
+
+        for (const specifier in j.expectedResults) {
+          const expected = j.expectedResults[specifier];
+          if (expected === null) {
+            expect(() => resolve(specifier, j.parsedImportMap, new URL(j.baseURL))).toThrow(TypeError);
+          } else {
+            // Should be resolved to `expected`.
+            expect(resolve(specifier, j.parsedImportMap, new URL(j.baseURL))).toMatchURL(expected);
+          }
+        }
+      });
+    }
+
+    // Parsing tests.
+    if ('expectedParsedImportMap' in j) {
+      it(j.name, () => {
+        if (j.expectedParsedImportMap === null) {
+          expect(j.parsedImportMap).toBeInstanceOf(TypeError);
+        } else {
+          expect(j.parsedImportMap)
+            .toEqual(replaceStringWithURL(j.expectedParsedImportMap));
+        }
+      });
+    }
+  }
+}
+
+exports.runTests = runTests;
---
+++ reference-implementation/__tests__/test.js
@@ -0,0 +1,28 @@
+'use strict';
+const { runTests } = require('./helpers/common-test-helper.js');
+for (const jsonFile of [
+  'data-base-url.json',
+  'empty-import-map.json',
+  'overlapping-entries.json',
+  'packages-via-trailing-slashes.json',
+  'parsing-addresses-absolute.json',
+  'parsing-addresses-invalid.json',
+  'parsing-addresses.json',
+  'parsing-invalid-json.json',
+  'parsing-schema-normalization.json',
+  'parsing-schema-scope.json',
+  'parsing-schema-specifier-map.json',
+  'parsing-schema-toplevel.json',
+  'parsing-scope-keys.json',
+  'parsing-specifier-keys.json',
+  'parsing-trailing-slashes.json',
+  'scopes-exact-vs-prefix.json',
+  'scopes.json',
+  'tricky-specifiers.json',
+  'url-specifiers.json'
+]) {
+  describe(jsonFile, () => {
+    const j = require('./json/' + jsonFile); // eslint-disable-line global-require
+    runTests(j);
+  });
+}
