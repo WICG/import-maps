8be56ee09701df844fa750dabd970d789cf10eb7
Domenic Denicola <d@domenic.me>
2020-10-22 11:45:55 -0400

    Reference implementation: test all JSON files
    The previous commit did not actually run the new tests.

--- reference-implementation/__tests__/test.js
+++ reference-implementation/__tests__/test.js
@@ -1,27 +1,9 @@
 'use strict';
+const fs = require('fs');
+const path = require('path');
 const { runTests } = require('./helpers/common-test-helper.js');
-for (const jsonFile of [
-  'data-base-url.json',
-  'empty-import-map.json',
-  'overlapping-entries.json',
-  'packages-via-trailing-slashes.json',
-  'parsing-addresses-absolute.json',
-  'parsing-addresses-invalid.json',
-  'parsing-addresses.json',
-  'parsing-invalid-json.json',
-  'parsing-schema-normalization.json',
-  'parsing-schema-scope.json',
-  'parsing-schema-specifier-map.json',
-  'parsing-schema-toplevel.json',
-  'parsing-scope-keys.json',
-  'parsing-specifier-keys.json',
-  'parsing-trailing-slashes.json',
-  'resolving-null.json',
-  'scopes-exact-vs-prefix.json',
-  'scopes.json',
-  'tricky-specifiers.json',
-  'url-specifiers.json'
-]) {
+
+for (const jsonFile of fs.readdirSync(path.resolve(__dirname, 'json'))) {
   describe(jsonFile, () => {
     const j = require('./json/' + jsonFile); // eslint-disable-line global-require
     runTests(j);
